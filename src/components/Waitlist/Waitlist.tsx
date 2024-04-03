53b2e22e3cb799057e4f425e8e34b96cd30bcdfb
Luca Restagno <luca.restagno@gmail.com>
2024-04-03 22:39:33 +0200

    fix(waitlist): remove data attributes for recaptcha that are not needed

--- src/components/Waitlist/Waitlist.tsx
+++ src/components/Waitlist/Waitlist.tsx
@@ -90,9 +90,6 @@ export const Waitlist = () => {
               minW="180px"
               onClick={onAddToWaitlist}
               isLoading={isLoading}
-              className="g-recaptcha"
-              data-sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}
-              data-action="submit"
             >
               Remind me
             </Button>
