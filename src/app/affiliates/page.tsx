49fea56ca21fa55e5e22d7f7f2110e2e351ab60d
Sid Mohan <sidmohan001@gmail.com>
2025-01-22 13:39:48 -0800

    added back affiliates

--- src/app/affiliates/page.tsx
+++ src/app/affiliates/page.tsx
@@ -1,16 +1,16 @@
-// import React from "react";
-// import { brandName } from "@/config";
-// import { Affiliates } from "@/components/pages/Affiliates/Affiliates";
-// import { getSEOTags } from "@/components/SEOTags/SEOTags";
-// import { Metadata } from "next";
+import React from "react";
+import { brandName } from "@/config";
+import { Affiliates } from "@/components/pages/Affiliates/Affiliates";
+import { getSEOTags } from "@/components/SEOTags/SEOTags";
+import { Metadata } from "next";
 
-// export const metadata: Metadata = getSEOTags({
-//   title: `Affiliates | ${brandName}`,
-//   description: `Earn 40% on each sale you refer to ${brandName}`,
-// });
+export const metadata: Metadata = getSEOTags({
+  title: `Affiliates | ${brandName}`,
+  description: `Earn 40% on each sale you refer to ${brandName}`,
+});
 
-// function AffiliatesPage() {
-//   return <Affiliates />;
-// }
+function AffiliatesPage() {
+  return <Affiliates />;
+}
 
-// export default AffiliatesPage;
+export default AffiliatesPage;
